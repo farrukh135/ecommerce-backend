f8941710e705b56ba9680dff416ec45d2ab82432
farrukh135 <afarrukh302@gmail.com>
2024-01-22 18:01:54 +0500

    Test 2

--- index.js
+++ index.js
@@ -173,7 +173,7 @@ server.post("/create-payment-intent", async (req, res) => {
   // Create a PaymentIntent with the order amount and currency
   const paymentIntent = await stripe.paymentIntents.create({
     amount: totalAmount * 100, // for decimal compensation
-    currency: "inr",
+    currency: "usd",
     automatic_payment_methods: {
       enabled: true,
     },
